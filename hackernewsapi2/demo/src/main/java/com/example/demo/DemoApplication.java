9e65e32f9f98c5423886477b4eae80aeca87ed0f
Enshinyan <emeka.enshinyan@gmail.com>
2021-10-21 01:14:11 -0700

    testing a simple get request

--- hackernewsapi2/demo/src/main/java/com/example/demo/DemoApplication.java
+++ hackernewsapi2/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.*;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
+import java.net.URI;
 import java.net.URL;
-import java.util.ArrayList;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 import static java.lang.System.in;
 
@@ -18,70 +20,13 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-
+		//TECHNIQUE 2: using java.net.http.httpclient. will handle asynchronous operations for you
+		HttpClient httpClient = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder().uri(URI.create("https://hacker-news.firebaseio.com/v0/item/28902662.json?print=pretty")).build();
+		httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+				.thenApply(HttpResponse::body)
+				.thenAccept(System.out::println)
+				.join();
 		System.out.println("YT Time Stamp (by minute): 60");
 	}
-
-	public static void getApiRequest() throws IOException {
-
-
-		URL getUrl = new URL("https://hacker-news.firebaseio.com/v0/item/28902662.json?print=pretty");
-		HttpURLConnection connection = (HttpURLConnection) getUrl.openConnection();
-
-		//setting request?
-		connection.setRequestMethod("GET");
-		//getting response?
-		int responseCode = connection.getResponseCode();
-		//shows successful connection if response 200
-		if (responseCode == HttpURLConnection.HTTP_OK){
-			BufferedReader art = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-			StringBuffer jsonResponseData = new StringBuffer();
-			String readLine = null;
-
-			//append response line by line
-			while ((readLine = art.readLine()) != null){
-				jsonResponseData.append(readLine);
-			}
-			in.close();
-			System.out.println("data appended: " + jsonResponseData.toString());
-		}else{
-			System.out.println(responseCode);
-		}
-	}
 }
-
-
-/*	public static void getApiRequest() throws IOException {
-
-		// Get 10th record data
-		URL getUrl = new URL("https://jsonplaceholder.typicode.com/posts/10");
-
-		HttpURLConnection conection = (HttpURLConnection) getUrl.openConnection();
-
-		// Set request method
-		conection.setRequestMethod("GET");
-
-		// Getting response code
-		int responseCode = conection.getResponseCode();
-
-		// If responseCode is 200 means we get data successfully
-		if (responseCode == HttpURLConnection.HTTP_OK) {
-			BufferedReader in = new BufferedReader(new InputStreamReader(conection.getInputStream()));
-			StringBuffer jsonResponseData = new StringBuffer();
-			String readLine = null;
-
-			// Append response line by line
-			while ((readLine = in.readLine()) != null) {
-				jsonResponseData.append(readLine);
-			}
-
-			in.close();
-			// Print result in string format
-			System.out.println("JSON String Data " + jsonResponseData.toString());
-		} else {
-			System.out.println(responseCode);
-		}
-
-	}
-
-}*/
