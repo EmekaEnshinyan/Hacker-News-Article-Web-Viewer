1db476ee313cb4f83d46429e1c1ffcbb8b3bb30b
Enshinyan <emeka.enshinyan@gmail.com>
2021-11-10 15:27:24 -0800

    edit: Successful GET request to Webpage

---
+++ src/main/java/com/example/hackerNewsApiToWeb/HackerNewsApiToWebApplication.java
@@ -0,0 +1,21 @@
+//TODO: The lines of code that are not understood have a numbered comment on its corresponding line
+
+package com.example.hackerNewsApiToWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.*;
+
+@SpringBootApplication
+public class HackerNewsApiToWebApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HackerNewsApiToWebApplication.class, args);
+
+	}
+
+
+
+}
---
+++ src/test/java/com/example/hackerNewsApiToWeb/HackerNewsApiToWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hackerNewsApiToWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackerNewsApiToWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
