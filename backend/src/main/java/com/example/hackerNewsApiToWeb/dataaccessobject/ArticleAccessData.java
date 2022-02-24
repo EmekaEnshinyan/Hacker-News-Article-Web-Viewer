87a551208706e594e334f05d16935bf781a1b88b
Enshinyan <emeka.enshinyan@gmail.com>
2022-02-23 22:24:02 -0800

    edit: moved files to backend folder

--- backend/src/main/java/com/example/hackerNewsApiToWeb/dataaccessobject/ArticleAccessData.java
+++ backend/src/main/java/com/example/hackerNewsApiToWeb/dataaccessobject/ArticleAccessData.java
@@ -3,7 +3,6 @@ package com.example.hackerNewsApiToWeb.dataaccessobject;
 import com.example.hackerNewsApiToWeb.model.Article;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
