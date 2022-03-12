841074f79c24e5bafe27a1f8c916abfb24d6955e
Enshinyan <emeka.enshinyan@gmail.com>
2022-03-11 20:48:51 -0800

    add: junit

---
+++ src/test/java/com/example/hackernewsapitoweb/controller/ArticleControllerTest.java
@@ -0,0 +1,17 @@
+package com.example.hackernewsapitoweb.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ArticleControllerTest {
+
+    @Autowired
+    private ArticleController articleController;
+    @Test
+    public void callDifferentHackerNewsArticle(String url){
+
+    }
+
+}
