8947280a6f6b580ec4ff905f8e01326f429c4935
Enshinyan <emeka.enshinyan@gmail.com>
2022-03-11 21:05:56 -0800

    edit: fix pull denial

--- src/test/java/com/example/hackernewsapitoweb/HackerNewsApiToWebApplicationTests.java
+++ src/test/java/com/example/hackernewsapitoweb/HackerNewsApiToWebApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class HackerNewsApiToWebApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void callDifferentHackerNewsArticle(String url) {
 	}
 
 }
